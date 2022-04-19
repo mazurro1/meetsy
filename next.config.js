882f2e99354b1767ad6af3983f66a7a8898c061f
hubert.mazur@nexio.pl <hubert.mazur@nexio.pl>
2022-04-19 15:26:25 +0200

    update texts

--- next.config.js
+++ next.config.js
@@ -18,9 +18,4 @@ module.exports = withPWA({
     ],
   },
   webpack5: true,
-  // webpack: (config) => {
-  //   config.resolve.fallback = {fs: false, path: false, process: false};
-
-  //   return config;
-  // },
 });
